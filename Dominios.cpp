3229086a3d57b291427e5956497955c445035b48
kalebhenrique <kalebhenriquebr@gmail.com>
2023-10-15 21:36:08 -0300

    adiciona validar Email

--- Dominios.cpp
+++ Dominios.cpp
@@ -4,6 +4,7 @@ void Codigo::validar(string valor) {
     if (valor.length() > LIMITE) {
         throw invalid_argument("Argumento invalido.");
     }
+
     for (int i = 0; i < valor.length(); i++) {
         if (i < 2) {
             if (valor[i] < 'A' || valor[i] > 'Z') {
@@ -25,7 +26,52 @@ void Coluna::validar(string valor) {
 }
 
 void Email::validar(string valor) {
+    string nome;
+    string dominio;
+
     if (valor.length() > LIMITE_NOME + LIMITE_DOMINIO + 1) {
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    if (valor.find('@') == string::npos) {  // verifica se existe @
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    if (valor.find('@') == 0 || valor.find('@') == valor.length() - 1) {
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    if (valor.find('@') != valor.rfind('@')) {  // verifica se ha dois @
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    if (valor.find('@') - 1 == '.' || valor.find('@') + 1 == '.') {
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    for (int i = 0; i < valor.length() - 1; i++) {
+        if (   valor[i] < 'A' || valor[i] > 'Z' 
+            && valor[i] < 'a' || valor[i] > 'z' 
+            && valor[i] < '0' || valor[i] > '9' 
+            && valor[i] != '.' 
+            && valor[i] != '@')
+        {
+            throw invalid_argument("Argumento invalido.");
+        }
+        if (valor[i] == '.' && valor[i + 1] == '.') {
+            throw invalid_argument("Argumento invalido.");
+        }
+    }
+
+    size_t posicaoArroba = valor.find('@');
+    nome = valor.substr(0, posicaoArroba);
+    dominio = valor.substr(posicaoArroba + 1, valor.length() - posicaoArroba - 1);
+
+    if (nome.length() < 2 || nome.length() > LIMITE_NOME) {
+        throw invalid_argument("Argumento invalido.");
+    }
+
+    if (dominio.length() < 2 || dominio.length() > LIMITE_DOMINIO) {
         throw invalid_argument("Argumento invalido.");
     }
 }
