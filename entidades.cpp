90037ffcfe757f53996b342619418d5882b4ca66
kalebhenrique <kalebhenriquebr@gmail.com>
2023-10-22 13:27:30 -0300

    testes de entidade passou

--- entidades.cpp
+++ entidades.cpp
@@ -1,92 +1,92 @@
 #include "entidades.h"
 
-// Conta
-inline Email Conta::getEmail() {
-    return email;
-}
+// // Conta
+// inline Email Conta::getEmail() {
+//     return email;
+// }
 
-void Conta::setEmail(const Email& email) {
-    this->email = email;
-}
+// void Conta::setEmail(const Email& email) {
+//     this->email = email;
+// }
 
-inline Texto Conta::getNome() {
-    return nome;
-}
+// inline Texto Conta::getNome() {
+//     return nome;
+// }
 
-void Conta::setNome(const Texto& nome) {
-    this->nome = nome;
-}
+// void Conta::setNome(const Texto& nome) {
+//     this->nome = nome;
+// }
 
-inline Senha Conta::getSenha() {
-    return senha;
-}
+// inline Senha Conta::getSenha() {
+//     return senha;
+// }
 
-void Conta::setSenha(const Senha& senha) {
-    this->senha = senha;
-}
+// void Conta::setSenha(const Senha& senha) {
+//     this->senha = senha;
+// }
 
-// Quadro
-inline Codigo Quadro::getCodigo() {
-    return codigo;
-}
+// // Quadro
+// inline Codigo Quadro::getCodigo() {
+//     return codigo;
+// }
 
-void Quadro::setCodigo(const Codigo& codigo) {
-    this->codigo = codigo;
-}
+// void Quadro::setCodigo(const Codigo& codigo) {
+//     this->codigo = codigo;
+// }
 
-inline Texto Quadro::getNome() {
-    return nome;
-}
+// inline Texto Quadro::getNome() {
+//     return nome;
+// }
 
-void Quadro::setNome(const Texto& nome) {
-    this->nome = nome;
-}
+// void Quadro::setNome(const Texto& nome) {
+//     this->nome = nome;
+// }
 
-inline Texto Quadro::getDescricao() {
-    return descricao;
-}
+// inline Texto Quadro::getDescricao() {
+//     return descricao;
+// }
 
-void Quadro::setDescricao(const Texto& descricao) {
-    this->descricao = descricao;
-}
+// void Quadro::setDescricao(const Texto& descricao) {
+//     this->descricao = descricao;
+// }
 
-inline Limite Quadro::getLimite() {
-    return limite;
-}
+// inline Limite Quadro::getLimite() {
+//     return limite;
+// }
 
-void Quadro::setLimite(const Limite& limite) {
-    this->limite = limite;
-}
+// void Quadro::setLimite(const Limite& limite) {
+//     this->limite = limite;
+// }
 
-// Cartao
-inline Codigo Cartao::getCodigo() {
-    return codigo;
-}
+// // Cartao
+// inline Codigo Cartao::getCodigo() {
+//     return codigo;
+// }
 
-void Cartao::setCodigo(const Codigo& codigo) {
-    this->codigo = codigo;
-}
+// void Cartao::setCodigo(const Codigo& codigo) {
+//     this->codigo = codigo;
+// }
 
-inline Texto Cartao::getNome() {
-    return nome;
-}
+// inline Texto Cartao::getNome() {
+//     return nome;
+// }
 
-void Cartao::setNome(const Texto& nome) {
-    this->nome = nome;
-}
+// void Cartao::setNome(const Texto& nome) {
+//     this->nome = nome;
+// }
 
-inline Texto Cartao::getDescricao() {
-    return descricao;
-}
+// inline Texto Cartao::getDescricao() {
+//     return descricao;
+// }
 
-void Cartao::setDescricao(const Texto& descricao) {
-    this->descricao = descricao;
-}
+// void Cartao::setDescricao(const Texto& descricao) {
+//     this->descricao = descricao;
+// }
 
-inline Coluna Cartao::getColuna() {
-    return coluna;
-}
+// inline Coluna Cartao::getColuna() {
+//     return coluna;
+// }
 
-void Cartao::setColuna(const Coluna& coluna) {
-    this->coluna = coluna;
-}
+// void Cartao::setColuna(const Coluna& coluna) {
+//     this->coluna = coluna;
+// }
--- entidades.h
+++ entidades.h
@@ -1,3 +1,6 @@
+#ifndef ENTIDADES_H_INCLUDED
+#define ENTIDADES_H_INCLUDED
+
 #include "dominios.h"
 
 class Conta {
@@ -47,3 +50,96 @@ class Cartao {
         Coluna getColuna();
         void setColuna(const Coluna&);
 };
+
+// Conta
+inline Email Conta::getEmail() {
+    return email;
+}
+
+inline void Conta::setEmail(const Email& email) {
+    this->email = email;
+}
+
+inline Texto Conta::getNome() {
+    return nome;
+}
+
+inline void Conta::setNome(const Texto& nome) {
+    this->nome = nome;
+}
+
+inline Senha Conta::getSenha() {
+    return senha;
+}
+
+inline void Conta::setSenha(const Senha& senha) {
+    this->senha = senha;
+}
+
+// Quadro
+inline Codigo Quadro::getCodigo() {
+    return codigo;
+}
+
+inline void Quadro::setCodigo(const Codigo& codigo) {
+    this->codigo = codigo;
+}
+
+inline Texto Quadro::getNome() {
+    return nome;
+}
+
+inline void Quadro::setNome(const Texto& nome) {
+    this->nome = nome;
+}
+
+inline Texto Quadro::getDescricao() {
+    return descricao;
+}
+
+inline void Quadro::setDescricao(const Texto& descricao) {
+    this->descricao = descricao;
+}
+
+inline Limite Quadro::getLimite() {
+    return limite;
+}
+
+inline void Quadro::setLimite(const Limite& limite) {
+    this->limite = limite;
+}
+
+// Cartao
+inline Codigo Cartao::getCodigo() {
+    return codigo;
+}
+
+inline void Cartao::setCodigo(const Codigo& codigo) {
+    this->codigo = codigo;
+}
+
+inline Texto Cartao::getNome() {
+    return nome;
+}
+
+inline void Cartao::setNome(const Texto& nome) {
+    this->nome = nome;
+}
+
+inline Texto Cartao::getDescricao() {
+    return descricao;
+}
+
+inline void Cartao::setDescricao(const Texto& descricao) {
+    this->descricao = descricao;
+}
+
+inline Coluna Cartao::getColuna() {
+    return coluna;
+}
+
+inline void Cartao::setColuna(const Coluna& coluna) {
+    this->coluna = coluna;
+}
+
+#endif // ENTIDADES_H_INCLUDED
